2fc4b06b8a52609f424c7c253392e894ad41fd11
Juan Bernal <mesieou@gmail.com>
2024-11-07 19:33:14 +1100

    linked list to arrray recursively

--- leetcode/linkedListRefresher/linkedList.py
+++ leetcode/linkedListRefresher/linkedList.py
@@ -14,10 +14,24 @@ b.next = c
 c.next = d
 d.next = e
 
-def linked_list_to_array(head):
-    if head == None: return []
+# def linked_list_to_array(head):
+#     if head == None: return []
 
-    return [head.val] + linked_list_to_array(head.next)
+#     return [head.val] + linked_list_to_array(head.next)
+
+# res = linked_list_to_array(a)
+
+def linked_list_to_array(head):
+    ans = []
+    def helper(head):
+        if head == None: return 
+        ans.append(head.val)
+        helper(head.next)
+    
+    helper(head)
+    return ans
+
+
 
 res = linked_list_to_array(a)
 print(res)
