004864cd3d7a9706e3c58b862363efbac95918af
Juan Bernal <mesieou@gmail.com>
2024-11-07 19:20:08 +1100

    linked list to array

---
+++ leetcode/linkedListRefresher/linkedList.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+def linked_list_to_array(head):
+    if head == None: return []
+
+    return [head.val] + linked_list_to_array(head.next)
+
+res = linked_list_to_array(a)
+print(res)
