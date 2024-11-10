384c33278feaaa4537f9a57135ed3fadcbbfdf1f
Juan David Bernal <125519096+mesieou@users.noreply.github.com>
2024-11-10 16:32:27 +1100

    Create file

---
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(!head || !head.next) return head
+    let dummy = new ListNode(0, head);
+    let odd = head, even = head.next, evenHead = even;
+
+    while(even != null && even.next){
+        //move odd
+        odd.next = even.next
+        odd = odd.next
+
+        //move even
+        even.next = odd.next
+        even = even.next
+    }
+    odd.next = evenHead;
+
+    return dummy.next;
+};
