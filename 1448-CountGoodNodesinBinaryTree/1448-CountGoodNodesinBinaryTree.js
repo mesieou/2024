a2a6e030f88e58cd6b7536db18c5030b8ec2b39c
Juan David Bernal <125519096+mesieou@users.noreply.github.com>
2024-11-27 11:35:08 +1100

    Create file

---
+++ 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    const dfs = (root, gratestNum) => {
+        if(root == null) return 0;
+        gratestNum = Math.max(gratestNum, root.val);
+
+        if(!root.left && !root.right && root.val >= gratestNum) return 1;
+
+        if (root.val >= gratestNum){
+            return 1 + dfs(root.left, gratestNum) + dfs(root.right, gratestNum);
+        } else{
+            return dfs(root.left, gratestNum) + dfs(root.right, gratestNum)
+        }
+    }
+    
+    return dfs(root, -Infinity)
+};
